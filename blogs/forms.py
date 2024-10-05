ecfd70f6210f5d3056edd4f9ebfea48347208613
skylike4u <skylike4u@gmail.com>
2024-10-05 09:57:20 +0900

    forms.py updateded

--- blogs/forms.py
+++ blogs/forms.py
@@ -8,8 +8,9 @@ class PostCreationForm(ModelForm):
     # medium-edior 사용을 위한 커스터마이징(해당 html태그에 editable 클래스를 넣어둔다. attrs(attributes)에 class랑 style같은 것은 넣는게 content 필드가 만들어질때 forms에서 설정해서 클래스랑 스타일을 미리 결정해준다.)
     content = forms.CharField(
         widget=forms.Textarea(
-            attrs={"class": "editable text-start", "style": "height: auto;"}
-        )
+            attrs={"class": "editable text-start", "style": "height: auto;"},
+        ),
+        label="◎ 컨텐츠 내용",
     )
 
     # 카테고리 필드를 다중 선택 가능하게 설정 (ModelMultipleChoiceField 사용)
@@ -17,6 +18,7 @@ class PostCreationForm(ModelForm):
         queryset=Category.objects.all(),
         required=True,  # 카테고리 선택 필수 처리
         widget=forms.CheckboxSelectMultiple,  # 여러 개의 카테고리를 선택 가능하게 체크박스로 렌더링
+        label="◎ 카테고리(*1개만 선택해주세요)",
     )
 
     class Meta:
@@ -29,3 +31,10 @@ class PostCreationForm(ModelForm):
             "file_upload",
             "featured_image",
         ]
+        # Alternatively, override labels in the Meta class (this applies to fields not customized above)
+        labels = {
+            "title": "◎ 제 목",
+            "excerpt": "◎ 요 약 (주요내용 발췌 or 짧게 부제목처럼)",
+            "file_upload": "◎ 파일 업로드",
+            "featured_image": "◎ 주요 이미지",
+        }
